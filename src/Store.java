792e3253b71bdbd71d2f4ca570cf61e27eb14b70
Abimbola Ogundipe <ogundipeabimbola9@gmail.com>
2023-06-09 23:55:40 +0100

    New Java learning

--- src/Store.java
+++ src/Store.java
@@ -1,27 +1,27 @@
-// // Class Instances
-// public class Store {
-//     // instance fields
-//     String productType;
-//     int inventoryCount;
-//     double inventoryPrice;
+// Class Instances
+public class Store {
+    // instance fields
+    String productType;
+    int inventoryCount;
+    double inventoryPrice;
     
-//     // constructor method
-//     public Store(String product, int count, double price) {
-//       productType = product;
-//       inventoryCount = count;
-//       inventoryPrice = price;
-//     }
+    // constructor method
+    public Store(String product, int count, double price) {
+      productType = product;
+      inventoryCount = count;
+      inventoryPrice = price;
+    }
     
-//     // main method
-//     public static void main(String[] args) {
-//       Store lemonadeStand = new Store("lemonade", 42, .99);
-//       Store cookieShop = new Store("cookies", 12, 3.75);
+    // main method
+    public static void main(String[] args) {
+      Store lemonadeStand = new Store("lemonade", 42, .99);
+      Store cookieShop = new Store("cookies", 12, 3.75);
       
-//       System.out.println("Our first shop sells " + lemonadeStand.productType + " at " + lemonadeStand.inventoryPrice + " per unit.");
+      System.out.println("Our first shop sells " + lemonadeStand.productType + " at " + lemonadeStand.inventoryPrice + " per unit.");
       
-//       System.out.println("Our second shop has " + cookieShop.inventoryCount + " units remaining.");
-//     }
-//   }
+      System.out.println("Our second shop has " + cookieShop.inventoryCount + " units remaining.");
+    }
+  }
 
 public class Store {
   // instance fields
